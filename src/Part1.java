b86c36dcf4d9ceef2b19e77410d9930d857298cc
16holson <hunterolson@mail.weber.edu>
2022-12-17 18:37:11 -0700

    Created Weighted Directed Graph

--- src/Part1.java
+++ src/Part1.java
@@ -1,65 +1,200 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Vector;
+import java.util.*;
 
 public class Part1
 {
+    static class Vertex
+    {
+        private String value;
+        private int flow;
+
+        public Vertex(String value, int flow)
+        {
+            this.value = value;
+            this.flow = flow;
+        }
+
+        public Vertex(String value)
+        {
+            this.value = value;
+            this.flow = 0;
+        }
+
+        public Vertex()
+        {
+        }
+
+        public String getValue()
+        {
+            return value;
+        }
+
+        public void setValue(String value)
+        {
+            this.value = value;
+        }
+
+        public int getFlow()
+        {
+            return flow;
+        }
+
+        public void setFlow(int flow)
+        {
+            this.flow = flow;
+        }
+
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj == this)
+            {
+                return true;
+            }
+            Vertex other = (Vertex) obj;
+            return Objects.equals(this.value, other.getValue());
+        }
+
+        @Override
+        public int hashCode()
+        {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result
+                    + ((value == null) ? 0 : value.hashCode());
+            return result;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Vertex{" +
+                    "value=" + value +
+                    ", flow=" + flow +
+                    '}';
+        }
+    }
+
     static class Edge
     {
-        int source;
-        int dest;
-        int weight;
-        public Edge(int source, int dest, int weight)
+        private Vertex source;
+        private Vertex dest;
+        private int weight;
+
+        public Edge(Vertex source, Vertex dest, int weight)
         {
             this.source = source;
             this.dest = dest;
             this.weight = weight;
         }
+
+        @Override
+        public String toString()
+        {
+            return "Edge{" +
+                    "source=" + source +
+                    ", dest=" + dest +
+                    ", weight=" + weight +
+                    '}';
+        }
     }
+
     static class Graph
     {
-        int vertices; //Number of vertices
-        LinkedList<Edge>[] adjList;
+        Map<Vertex, List<Edge>> adj;
 
-        Graph(int vertices)
+        Graph()
         {
-            this.vertices = vertices;
-            this.adjList = new LinkedList[vertices];
-            for (int i = 0; i < vertices; i++)
+            adj = new HashMap<>();
+        }
+
+        public Map<Vertex, List<Edge>> getAdj()
+        {
+            return adj;
+        }
+
+        public void addVertex(String value, int flow)
+        {
+            Vertex v = new Vertex(value, flow);
+            List<Edge> edges = new ArrayList<>();
+            this.adj.put(v, edges);
+        }
+
+        public List<Edge> getEdges(Vertex source)
+        {
+            return adj.get(source);
+        }
+
+        public void addEdge(Vertex source, Vertex dest, int weight)
+        {
+            List<Edge> sourceEdges = this.getEdges(source);
+            sourceEdges.add(new Edge(source, dest, weight));
+        }
+
+        public void printGraph()
+        {
+            for (Map.Entry<Vertex, List<Edge>> entry : adj.entrySet())
             {
-                this.adjList[i] = new LinkedList<>();
+                System.out.println(entry.getKey() + ":" + entry.getValue().toString());
             }
         }
 
-        public void addEdge(int source, int dest, int weight)
+        public void traverse()
         {
-            Edge edge = new Edge(source, dest, weight);
-            adjList[source].addFirst(edge);
+
         }
-        public void printGraph()
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        Graph graph = new Graph();
+        String line;
+        String file = new File("").getAbsolutePath();
+        file = file.concat("\\src\\InputData.txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        while ((line = bufferedReader.readLine()) != null)
         {
-            for(int i = 0; i < vertices; i++)
+            String[] split = line.strip().split("; ");
+            String value = split[0].replace("Valve ", "").replace(" has flow rate=", "").replaceAll("\\d", "");
+            int flow = Integer.parseInt(split[0].replaceAll("[^\\d]", ""));
+
+            graph.addVertex(value, flow);
+
+        }
+        bufferedReader = new BufferedReader(new FileReader(file));
+        while ((line = bufferedReader.readLine()) != null)
+        {
+
+            String[] split = line.strip().split("; ");
+            String value = split[0].replace("Valve ", "").replace(" has flow rate=", "").replaceAll("\\d", "");
+            int flow = Integer.parseInt(split[0].replaceAll("[^\\d]", ""));
+            Vertex vertex = new Vertex(value, flow);
+            String replace = split[1].strip().replace("tunnels lead to valves ", "");
+            String[] edges;
+            if (!replace.contains(","))
             {
-                LinkedList<Edge> list = adjList[i];
-                for (Edge edge : list)
+                edges = new String[]{replace.replace("tunnel leads to valve ", "")};
+            }
+            else
+            {
+                edges = replace.strip().split(", ");
+            }
+            for (Vertex temp : graph.getAdj().keySet())
+            {
+                for (String edge : edges)
                 {
-                    System.out.println("vertex-" + i + " is connected to " + edge.dest + " with weight " + edge.weight);
+                    if (temp.getValue().equals(edge))
+                    {
+                        graph.addEdge(vertex, temp, 2);
+                    }
+
                 }
             }
         }
-
-        public static void main(String[] args) throws IOException
-        {
-            int vertices = 10;
-            Graph graph = new Graph(vertices);
-            String line;
-            String file = new File("").getAbsolutePath();
-            file = file.concat("\\src\\InputData.txt");
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            while((line = bufferedReader.readLine()) != null)
-            {
-
-            }
-        }
+        graph.printGraph();
     }
 }
